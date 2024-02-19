e5814c3d537c5a532675af2fa59cf64cab4fd184
Jeroen van den Hoven <jeroen.vandenhoven@adidas.com>
2024-02-19 08:42:44 +0100

    Fix java and spark installation locations

--- install.sh
+++ install.sh
@@ -22,3 +22,16 @@ brew install bash-git-prompt
 
 # Git completion
 brew install git bash-completion
+
+# Java
+brew install java11 wget
+
+# Spark
+# Find the right version needed here: https://archive.apache.org/dist/spark/
+export SPARK_VERSION=3.2.1
+export HADOOP_VERSION=2.7
+export SPARK_DIR=~/.spark/$SPARK_VERSION
+mkdir -p $SPARK_DIR
+wget https://archive.apache.org/dist/spark/spark-$SPARK_VERSION/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz -O $SPARK_DIR/spark.tgz
+tar -xzf $SPARK_DIR/spark.tgz -C $SPARK_DIR
+rm $SPARK_DIR/spark.tgz
